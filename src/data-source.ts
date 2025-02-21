3ef0844b66cb24aa1806fc1a244e79876544cf23
Korede Anunusor <koredeanunusor@gmai.com>
2025-02-21 13:06:26 +0100

    typeORM setup

---
+++ src/data-source.ts
@@ -0,0 +1,16 @@
+import "reflect-metadata"
+import { DataSource } from "typeorm"
+import { User } from "./entity/User"
+
+export const AppDataSource = new DataSource({
+    type: "mongodb",
+    host: "localhost",
+    port: 27017,
+    password: "",
+    database: "test",
+    synchronize: true,
+    logging: false,
+    entities: [User],
+    migrations: [],
+    subscribers: [],
+})
